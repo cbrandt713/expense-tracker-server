691802a96cd3b98c7e08222293c5d94f6da63283
cbrandt713 <cbrandt@ramapo.edu>
2021-05-27 23:24:14 -0400

    Add app scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExpenseModule } from './expense/expense.module';
+import { UserModule } from './user/user.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [],
+  imports: [ExpenseModule, UserModule, GroupModule],
   controllers: [AppController],
   providers: [AppService],
 })
