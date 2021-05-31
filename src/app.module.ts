6a0330402738a0e3b2a894f9458e4d5d8cce09ac
cbrandt713 <cbrandt@ramapo.edu>
2021-05-31 16:38:11 -0400

    Update README, remove unneeded files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ExpenseModule } from './expense/expense.module';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DebtModule } from './debt/debt.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(), ExpenseModule, UserModule, GroupModule, DebtModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [TypeOrmModule.forRoot(), ExpenseModule, UserModule, GroupModule, DebtModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
