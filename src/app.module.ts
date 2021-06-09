0be5742699a6db13bbe75faf0f992d41d81c3505
cbrandt713 <cbrandt@ramapo.edu>
2021-06-09 12:28:15 -0400

    Remove unused module import, fix database connect (old one was timed out)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,8 @@ import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DebtModule } from './debt/debt.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(), ExpenseModule, UserModule, GroupModule, DebtModule, ConfigModule.forRoot()],
+    imports: [TypeOrmModule.forRoot(), ExpenseModule, UserModule, GroupModule, DebtModule],
 })
 export class AppModule {}
